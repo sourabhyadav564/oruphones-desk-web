2e7b27e1d3492a851018eb4034415dc1f8b91e8d
Aman Khandelwal <aman@zenro.co.jp>
2022-06-15 23:24:20 +0530

    change in mobile web version redirection

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ module.exports = {
               "/Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/",
           },
         ],
-        destination: "https://m.oruphones.com/:path*",
+        destination: "https://oru-phones-mobile-web.vercel.app/:path*",
         permanent: false,
       },
     ];
