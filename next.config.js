6b34129f2c4bcb974ce90e499485e2fa9d932c63
Aman Khandelwal <aman@zenro.co.jp>
2022-06-16 12:40:53 +0530

    change in ContentSecurityPolicy in next.config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src example.com;
+  style-src 'self' example.com;
+  font-src 'self';  
+`
 module.exports = {
   reactStrictMode: true,
   async redirects() {
@@ -17,6 +24,20 @@ module.exports = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "header",
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+          },
+        ],
+      },
+    ];
+  },
   images: {
     domains: [
       "zenrodevimages.s3.us-west-2.amazonaws.com",
@@ -26,7 +47,7 @@ module.exports = {
       "mobiruecom.s3.us-west-2.amazonaws.com",
       "zenroecom.s3.us-west-2.amazonaws.com",
       "zenrodeviceimages.s3-us-west-2.amazonaws.com",
-      "demo-bucket-c2c-001.s3.amazonaws.com"
+      "demo-bucket-c2c-001.s3.amazonaws.com",
     ],
   },
 };
