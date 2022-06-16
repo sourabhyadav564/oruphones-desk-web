ad1d33415f6f12d48b287549b56e437b1125ed54
Aman Khandelwal <aman@zenro.co.jp>
2022-06-16 12:44:36 +0530

    change in ContentSecurityPolicy in next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
+// Before defining your Security Headers
+// add Content Security Policy directives using a template string.
+
 const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self';
   child-src example.com;
   style-src 'self' example.com;
   font-src 'self';  
-`
+`;
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+];
 module.exports = {
   reactStrictMode: true,
   async redirects() {
@@ -27,14 +36,9 @@ module.exports = {
   async headers() {
     return [
       {
+        // Apply these headers to all routes in your application.
         source: "/:path*",
-        has: [
-          {
-            type: "header",
-            key: "Content-Security-Policy",
-            value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
-          },
-        ],
+        headers: securityHeaders,
       },
     ];
   },
