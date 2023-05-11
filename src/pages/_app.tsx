9a7850e3ba089907e28ce3179a3c3a10b6d8a96e
Abishek Devendran <abishekdevendran@gmail.com>
2023-05-11 11:08:52 +0530

    State management/sync setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,51 +4,63 @@ import '../styles/globals.css';
 import '../styles/react-slick/react-slick.css';
 import { AuthProvider } from '@/context/AuthContext';
 import { ApplicationContext } from '../context/ApplicationContext';
-import { useRouter } from 'next/router';
 import { RecoilRoot } from 'recoil';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AppProps } from 'next/app';
 import SEO from '@/data/seoOptions';
 import { DefaultSeo } from 'next-seo';
+import { Provider } from 'jotai';
+import { queryClientAtom } from 'jotai-tanstack-query';
+import { useHydrateAtoms } from 'jotai/react/utils'; // for SSR support
+import { PropsWithChildren } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'; // is not part of the build bundle, purely for Dev purposes
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: Infinity,
+		},
+	},
+});
+
+const HydrateAtoms = ({ children }: PropsWithChildren<{}>) => {
+	useHydrateAtoms([[queryClientAtom, queryClient]]);
+	return <>{children}</>;
+};
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	const router = useRouter();
-	if (router.pathname != '/i/[someId]') {
-		return (
-			<>
-				<AuthProvider>
-					<ApplicationContext>
+	return (
+		<>
+			<AuthProvider>
+				<ApplicationContext>
+					<QueryClientProvider client={queryClient}>
+						<ReactQueryDevtools />
 						<RecoilRoot>
-							<Header />
-							<DefaultSeo {...SEO} />
-							<Component {...pageProps} />
-							<Footer />
+							<Provider>
+								<HydrateAtoms>
+									<Header />
+									<DefaultSeo {...SEO} />
+									<Component {...pageProps} />
+									<Footer />
+								</HydrateAtoms>
+							</Provider>
 						</RecoilRoot>
-					</ApplicationContext>
-					<ToastContainer
-						position="bottom-center"
-						autoClose={3000}
-						hideProgressBar={false}
-						newestOnTop={false}
-						closeOnClick
-						rtl={false}
-						pauseOnFocusLoss
-						draggable
-						pauseOnHover
-					/>
-				</AuthProvider>
-			</>
-		);
-	} else {
-		return (
-			<>
-				<AuthProvider>
-					<ApplicationContext>
-						<Component {...pageProps} />
-					</ApplicationContext>
-				</AuthProvider>
-			</>
-		);
-	}
+					</QueryClientProvider>
+				</ApplicationContext>
+				<ToastContainer
+					position="bottom-center"
+					autoClose={3000}
+					hideProgressBar={false}
+					newestOnTop={false}
+					closeOnClick
+					rtl={false}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+				/>
+			</AuthProvider>
+		</>
+	);
 }
