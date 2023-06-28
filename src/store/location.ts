e0f5d1673b3a1d0f416d3de78030e0a8dda0a0d9
vishnuoruphones <vishnu@oruphones.com>
2023-06-28 10:39:29 +0530

    updated the use current location with new atom

--- src/store/location.ts
+++ src/store/location.ts
@@ -25,24 +25,29 @@ export const updateLocationAtom = atom(
 
 export const updateNewLocationAtom = atom(
 	null,
-	async (_get, set, locationObj: {
-		locality: string;
-		city: string;
-		state: string;
-		latitude: number;
-		longitude: number;
-		location : string;
-	}) => {
-		const { locality, city, state, latitude, longitude,location } = locationObj;
+	async (
+		_get,
+		set,
+		locationObj: {
+			locality: string;
+			city: string;
+			state: string;
+			latitude: number;
+			longitude: number;
+			location: string;
+		}
+	) => {
+		const { locality, city, state, latitude, longitude, location } =
+			locationObj;
 		set(localityAtom, locality);
 		set(cityAtom, city);
 		set(locationAtom, location);
 		set(latitudeAtom, latitude);
 		set(longitudeAtom, longitude);
-		set(stateAtom, state)
+		set(stateAtom, state);
 
 		setCookie('locality', locality);
-		setCookie('state', state)
+		setCookie('state', state);
 		setCookie('city', city);
 		setCookie('location', location);
 		setCookie('latitude', latitude.toString());
@@ -70,6 +75,7 @@ export const updateLocationLatLongAtom = atom(
 		console.log(address);
 		let city;
 		let state;
+		let locality;
 		for (let i = 0; i < address.address_components.length; i++) {
 			for (let j = 0; j < address.address_components[i].types.length; j++) {
 				switch (address.address_components[i].types[j]) {
@@ -80,12 +86,28 @@ export const updateLocationLatLongAtom = atom(
 					case 'administrative_area_level_1':
 						state = address.address_components[i].long_name;
 						break;
+					case 'sublocality_level_1':
+						locality = address.address_components[i].long_name;
+						break;
 				}
 			}
 		}
 		const statewithcity = city.replace(/\s/g, '') + ',' + ' ' + state;
 
+		const lat = address.geometry.location.lat
+		const long =address.geometry.location.lng
+
+		let locationObj = {
+			locality: locality,
+			city: city,
+			state: state,
+			latitude: lat,
+			longitude: long,
+			location: statewithcity,
+		};
+
 		set(updateLocationAtom, statewithcity);
+	set(updateNewLocationAtom, locationObj)
 	}
 );
 
